acce3a7af131801c00b5037cca3249ad9be33906
Beth <Beth3346@gmail.com>
2021-12-05 18:34:58 -0600

    add faq accordion

--- src/script.js
+++ src/script.js
@@ -1,1 +1,30 @@
 "use-strict";
+(() => {
+  const accordions = Array.from(
+    document.getElementsByClassName("faq-accordion")
+  );
+  console.log({ accordions });
+  accordions.forEach(accordion => {
+    const buttons = Array.from(accordion.getElementsByTagName("button"));
+
+    buttons.forEach(button =>
+      button.addEventListener("click", () => {
+        const contentId = button.attributes["data-contentid"].nodeValue;
+        const content = document.getElementById(contentId);
+        const expanded = button.attributes["aria-expanded"].nodeValue;
+
+        if (content) {
+          content.classList.toggle("active");
+        }
+
+        button.classList.toggle("active");
+
+        if (expanded === "false") {
+          button.attributes["aria-expanded"].nodeValue = "true";
+        } else {
+          button.attributes["aria-expanded"].nodeValue = "false";
+        }
+      })
+    );
+  });
+})();
